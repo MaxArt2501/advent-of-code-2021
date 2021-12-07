217ec397d2c1e6f6dd693da66a737efebd156bc8
Massimo Artizzu <maxart.x@gmail.com>
2021-12-07 08:48:34 +0100

    Day 7

---
+++ day-7/part-1-2.js
@@ -0,0 +1,22 @@
+const positions = input.split(',').map(Number);
+const minPos = Math.min(...positions);
+const maxPos = Math.max(...positions);
+
+const linearConsumptions = Array.from(
+  { length: maxPos - minPos + 1 },
+  (_, pos) => positions.reduce((total, crabPos) => total + Math.abs(pos - crabPos), 0)
+);
+
+console.log(Math.min(...linearConsumptions));
+
+const triangularConsumptions = Array.from(
+  { length: maxPos - minPos + 1 },
+  (_, pos) => positions.reduce((total, crabPos) => {
+    const diff = Math.abs(pos - crabPos);
+    /** Note: the sum of the integers from 0 to n is n*(n + 1)/2
+     * @see {@link https://en.wikipedia.org/wiki/Triangular_number} */
+    return total + diff * (diff + 1) / 2;
+  }, 0)
+);
+
+console.log(Math.min(...triangularConsumptions));
